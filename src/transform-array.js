483cfda3a7fe4e31db4618e9ba586300126e9f1a
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-02-17 22:59:47 +0100

    feat: implementation transform function (Mon, Feb 17, 2025 22:59:35 PM)

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,75 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!(arr instanceof Array)){
+    throw new Error ("'arr' parameter must be an instance of the Array!");
+  }
+  let transformedArray = [...arr];
+ 
+  
+  let discardNext = (arrC, index) => {
+    if(index === arrC.length - 1){
+      arrC[index] = undefined;
+    }
+    if (index + 1 < arrC.length) {
+      arrC[index] = undefined; 
+      arrC[index + 1] = undefined; 
+  }
+  }
+  
+  let discardPrev = (arrC, index) => {
+    if(index === 0){
+      arrC[index] = undefined;
+    }
+    if (index - 1 >= 0 ) {
+      arrC[index] = undefined; 
+      arrC[index - 1] = undefined; 
+    }
+  }
+  
+   let doubleNext = (arrC, index) => {
+   
+    if (index + 1 < arrC.length) {
+      arrC[index] = arrC[index + 1];
+    }
+     if(index === arrC.length - 1){
+      arrC[index] = undefined;
+    }
+  }
+  
+   let doublePrev = (arrC, index) => {
+    if (index - 1 >= 0) {
+      arrC[index] = arrC[index - 1];
+    } else {
+      arrC[index] = undefined;
+    }
+  }
+
+  let states = {
+      '--discard-next': discardNext,
+      '--discard-prev': discardPrev,
+      '--double-next': doubleNext,
+      '--double-prev': doublePrev
+  }
+  
+
+  
+  let iteractionArray = (array) => {
+      console.log(array)
+        let tempArr = array.slice();
+       
+    for (let i = 0; i < tempArr.length; i++) {
+        console.log(tempArr)
+      if (states[tempArr[i]]) {
+        states[tempArr[i]](tempArr, i);
+      }
+    
+    }
+    return tempArr.filter(el => el !== undefined);
+  }
+
+ return  iteractionArray(transformedArray)
 }
 
 module.exports = {
