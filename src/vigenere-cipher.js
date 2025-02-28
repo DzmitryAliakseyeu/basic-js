44e3c6cec3c96e849a399b54aed73ca20d62d3cd
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-02-28 14:51:36 +0100

    feat: implementation decript method for VigenereCipheringMachine class (Fri, Feb 28, 2025 14:51:12 PM)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,6 +20,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
+  constructor(isDirect){
+    this.isDirect = isDirect;
+  }
+
   encrypt(word, key) {
     if(!word || !key ){
       throw Error ('Incorrect arguments!');
@@ -44,11 +48,41 @@ class VigenereCipheringMachine {
       }
     }
 
-    return res.join('')
+    if(this.isDirect){
+      return res.reverse().join('');
+    } else {
+      return res.join('');
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(word, key) {
+    if(!word || !key ){
+      throw Error ('Incorrect arguments!');
+    }
+    word = word.toUpperCase()
+    key = key.toUpperCase()
+    let alph ="abcdefghijklmnopqrstuvwxyz".toUpperCase()
+    let res = [];
+    let ik = 0;
+    for(let i = 0; i < word.length; i += 1){
+      let currentLetter = word[i];
+      if(!alph.includes(currentLetter)){
+        res.push(currentLetter)
+      } else {
+        if(ik >= key.length){
+          ik = 0
+        }
+        let currentLetterInd = alph.indexOf(currentLetter)
+        let ciphLetterInd = alph.indexOf(key[ik]);
+        res.push(alph[(currentLetterInd - ciphLetterInd  + 26) % 26] )
+        ik += 1
+      }
+    }
+
+    if(this.isDirect){
+      return res.reverse().join('');
+    } else {
+      return res.join('');
+    }
   }
 }
 
